7c4ce43cd2323eeb423728a70f5a9a897e008124
kevin <kevin@agideo.com>
2015-08-27 22:20:06 +0800

    2

--- 3_set_vim_terminator.sh
+++ 3_set_vim_terminator.sh
@@ -3,8 +3,11 @@ set -v on
 
 #设定vim配置文件链接
 Cur_dir=$(pwd)
+rm -rf ~/.vimrc
 ln -sf $Cur_dir/.vimrc ~/.vimrc
+rm -rf ~/.vim/colors
 ln -sf $Cur_dir/colors ~/.vim/colors
+rm -rf ~/.config/terminator
 ln -sf $Cur_dir/.config/terminator ~/.config/terminator
 
 #通过Vundle安装vim插件
